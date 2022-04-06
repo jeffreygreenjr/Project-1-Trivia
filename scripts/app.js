de8ef1abb129bcf6e4345e47a052165b20f69985
jeffreygreen <mrjeffreygreen@gmail.com>
2022-04-06 15:26:21 -0500

    Added divs in HTML for questions/answers as well as global jQuery variables in js

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
 // console.log("Hello world!")
 
-//Global Variables
+// Global Variables
+
+let $homeScreen = $('.homeScreen')
+let $gameTitle = $('.gameTitle')
+
+let $playingScreen = $('.playingScreens')
+
+let $codingCategory = $('.codingCategory')
+let $sportsCategory = $('.sportsCategory')
+let $musicCategory = $('.musicCategory')
+
+let $questions = ${'.questions'}
+let $codingquestions = $('#codingquestions')
+let $sportsquestions = $('#sportsquestions')
+let $musicquestions = $('#musicquestions')
+
+let $answers = $('.answers')
+let $codinganswers = $('#codinganswers')
+let $sportsanswers = $('#sportsanswers')
+let $musicanswers = $('#musicanswers')
+
+let $wrongAnswer = $('.wrongAnswer')
